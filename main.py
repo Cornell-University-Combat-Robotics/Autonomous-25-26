3c358c4f888c367a8c7fb77303938905abf46c67
AHarnish <aaronjharnish@gmail.com>
2025-08-10 14:45:28 +0000

    Built main.py on main branch (BADgit add .git add .git add .git add . DONT DO THAT)

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+print("Huey main control system starting...")
+
+num = np.random.randint(1, 11)
+
+if num == 7:
+    print("Deleting system32...")
+else:
+    print("Attacking the enemy!!!!!!!!")
