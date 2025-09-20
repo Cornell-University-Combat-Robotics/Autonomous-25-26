d98063a2c7ff3f9da5242fa29746a0cdc84a2a7f
Ananya-Jajodia <nya.jaj@outlook.com>
2025-09-20 11:54:06 -0400

    espnow

--- transmission/esp32_code/imu_proccessor.py
+++ transmission/esp32_code/imu_proccessor.py
@@ -2,5 +2,6 @@ import serial
 
 ser = serial.Serial("COM3", 115200, timeout=1)  # use /dev/ttyUSB0 on Linux
 ser.write(b"get_imu\n")
-line = ser.readline().decode().strip()
+line = ser.readline().decode('utf-8').strip()
+
 print("IMU Data:", line)
