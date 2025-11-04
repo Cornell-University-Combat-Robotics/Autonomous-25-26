1e0bdf9b55e4c4ec94d5514f6d86023659f1c7b9
Ananya-Jajodia <nya.jaj@outlook.com>
2025-11-04 18:08:50 -0500

    Code review changes finished

--- sensors/imu_class_test.py
+++ sensors/imu_class_test.py
@@ -6,14 +6,13 @@ import imu_class as imu_class
 
 windows = "COM3"
 mac = "/dev/tty.usbserial-0001"
-# ser = serial.Serial(mac, 115200, timeout=1)
 
 sensor = imu_class.IMU_sensor()
 
 while True:
     try:
         print(f"yaw: {sensor.get_yaw()}")
-        print(f"is upside down: {sensor.is_upside_down()}")
+        # print(f"is upside down: {sensor.is_upside_down()}")
     except KeyboardInterrupt:
         break 
     except Exception as e:
