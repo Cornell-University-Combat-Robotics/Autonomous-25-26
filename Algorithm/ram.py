Fixed arrow display signs (inverted y-axis)

Algorithm/ram.py
@@ -172,9 +172,18 @@ class Ram():
             print("ENEMY ORIENTATION STUFF!")
 
             dx = cur_pos[0] - prev_pos[0]
-            dy = cur_pos[1] - prev_pos[1]
+            dy = -1 * (cur_pos[1] - prev_pos[1])
+
+            print(f"dx💩 {dx}💩")
+            print(f"dy💩 {dy}💩")
+
+            print(f"ARCY💩 {np.arctan2(dy,dx)}💩")
 
             orientation = np.degrees(np.arctan2(dy,dx))
+
+            # if orientation < 0:
+            #     orientation += 360
+            
             print(f"❤️traj: {orientation}❤️")
             return orientation
         return self.enemy_old_orientation

main_helpers.py
@@ -173,7 +173,7 @@ def display_angles(detected_bots_with_data, move_dictionary, image, enemy_orient
         if detected_bots_with_data["enemy"]:
             # Components of enemy front arrow
             dx = np.cos(math.pi / 180 * enemy_orientation)
-            dy = np.sin(math.pi / 180 * enemy_orientation) #TODO is orientation really 0-360.
+            dy = -1 * np.sin(math.pi / 180 * enemy_orientation)
 
             # Enemy's center
             start_x_enemy = int(detected_bots_with_data["enemy"]["center"][0])