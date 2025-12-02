5c90c4382903e67c7c10503eaf177472cc2e8cec
sophc <sophiecheng181@gmail.com>
2025-12-02 17:29:07 -0500

    Adjusted corner detection to be percent based

--- corner_detection/corner_detection_helpers.py
+++ corner_detection/corner_detection_helpers.py
@@ -23,6 +23,9 @@ def find_bot_color_pixels(image: np.ndarray, bot_color_hsv: list) -> int:
     mask = cv2.inRange(hsv_image, lower_limit, upper_limit)
 
     # Count the number of non-zero pixels in the mask
+    # cv2.imshow("Mask", mask)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
     return cv2.countNonZero(mask)
 
 def get_contours_per_color(side: str, hsv_image: np.ndarray, selected_colors) -> list[np.ndarray]:
@@ -62,7 +65,7 @@ def find_our_bot(images: list[np.ndarray], bot_color_hsv) -> tuple[np.ndarray | 
         if not images:
             raise ValueError("The input image list is empty.")
         
-        max_color_pixels = -1
+        max_color_percentage = -1
         our_bot_image = None
 
         for image in images:
@@ -75,15 +78,20 @@ def find_our_bot(images: list[np.ndarray], bot_color_hsv) -> tuple[np.ndarray | 
                 continue
 
             color_pixel_count = find_bot_color_pixels(image, bot_color_hsv)
-            print(color_pixel_count)
+            image_area = image.size
+            color_percentage = color_pixel_count/image_area
+            print(color_percentage)
 
-            if color_pixel_count > max_color_pixels:
+            if color_percentage > max_color_percentage:
                 our_bot_image = image
-                max_color_pixels = color_pixel_count
+                max_color_percentage = color_percentage
         
         if our_bot_image is None:
             print("Huey is not found")
             
+        # cv2.imshow("OUR BOT!", our_bot_image)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
         return our_bot_image
     
     except Exception as e:
@@ -107,9 +115,11 @@ def find_centroids_per_color(side: str, image: np.ndarray, hsv_image: np.ndarray
     centroids = []
     for contour in contours:
         # Filter out small contours based on area
-        area = cv2.contourArea(contour)
-        print("Area", area)
-        if area > 10:
+        image_area = image.size
+        contour_area = cv2.contourArea(contour)
+        contour_percent = contour_area/image_area
+        # print("Area", area)
+        if contour_percent > 0.005: # area > 20
             # TODO: this value is subject to change based on dimensions of our video & resize_factor
             # Compute moments for each contour
             M = cv2.moments(contour)
