1a8ca64dea9b080812b16449f62b79239b2b854f
vinsonchen3 <vinsonc0101@gmail.com>
2026-01-14 14:22:57 -0500

    pushed lower limit up for findbotcolorpixels wayyyyy better

--- corner_detection/corner_detection_helpers.py
+++ corner_detection/corner_detection_helpers.py
@@ -19,7 +19,7 @@ def find_bot_color_pixels(image: np.ndarray, bot_color_hsv: list) -> int:
     hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
     # Define the HSV range for the robot's color
-    lower_limit = np.array([max(0, bot_color_hsv[0] - 3), 75, 125])
+    lower_limit = np.array([max(0, bot_color_hsv[0] - 3), 125, 125])
     upper_limit = np.array([min(179, bot_color_hsv[0] + 3), 255, 255])
 
     # Create a mask for the robot's color in the image
