Changed image size to be accurate and also fixed an overflow error in color value

corner_detection/corner_detection_helpers.py
@@ -17,9 +17,13 @@ def find_bot_color_pixels(image: np.ndarray, bot_color_hsv: list) -> int:
     """
     hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
+    print("bot color type because of overflow error:")
+
+    print(type(bot_color_hsv[0]), bot_color_hsv.dtype)
+
     # Define the HSV range for the robot's color
-    lower_limit = np.array([max(0, bot_color_hsv[0] - 10), 50, 50])
-    upper_limit = np.array([min(179, bot_color_hsv[0] + 10), 255, 255])
+    lower_limit = np.array([max(0, bot_color_hsv[0] - 3), 75, 75])
+    upper_limit = np.array([min(179, bot_color_hsv[0] + 3), 255, 255])
 
     # Create a mask for the robot's color in the image
     mask = cv2.inRange(hsv_image, lower_limit, upper_limit)
@@ -84,7 +88,7 @@ def find_our_bot(images: list[np.ndarray], bot_color_hsv) -> tuple[np.ndarray | 
                 continue
 
             color_pixel_count = find_bot_color_pixels(image, bot_color_hsv)
-            image_area = image.size
+            image_area = image.size/3
             color_percentage = color_pixel_count/image_area
             print(color_percentage)
 
@@ -121,7 +125,7 @@ def find_centroids_per_color(side: str, image: np.ndarray, hsv_image: np.ndarray
     centroids = []
     for contour in contours:
         # Filter out small contours based on area
-        image_area = image.size
+        image_area = image.size/3
         contour_area = cv2.contourArea(contour)
         contour_percent = contour_area/image_area
         if contour_percent > 0.005: # area > 20
@@ -135,6 +139,8 @@ def find_centroids_per_color(side: str, image: np.ndarray, hsv_image: np.ndarray
                 centroids.append((cx, cy))
                 cv2.circle(image, (cx, cy), 8, (0, 0, 0), -1)
                 cv2.putText(image, side, (cx + 10, cy - 10), FONT, 0.5, (255, 255, 0), 2)
+                # cv2.imshow("Centroids", image)
+                # cv2.waitKey(0)
     return centroids
 
 def find_centroids(image: np.ndarray, selected_colors) -> np.ndarray: