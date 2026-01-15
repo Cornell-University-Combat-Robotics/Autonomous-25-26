20feb08f0740e2a1fc75cca1d8510f77768d62ce
Riddhi Lamba <rl893@cornell.edu>
2026-01-15 11:24:06 -0500

    deciding between recovery and sequential

--- main.py
+++ main.py
@@ -25,12 +25,12 @@ MATT_LAPTOP = False             # True if running on Matt's laptop
 JANK_CONTROLLER = False         # True if using backup controller
 COMP_SETTINGS = False           # Competition mode (no visuals, optimized speed)
 WARP_AND_COLOR_PICKING = True   # Re-do warp & color selection
-IS_TRANSMITTING = True          # True if connected to live Huey
+IS_TRANSMITTING = False          # True if connected to live Huey
 SHOW_FRAME = True               # Show camera feed frames
 IS_ORIGINAL_FPS = False         # Process every captured frame
 DISPLAY_ANGLES = SHOW_FRAME     # Only show angles if frames a
 UNSHARP_MASK = False            # True if unsharp mask is onre displayed
-CAN_RECOVER = False             # True if want recovery
+CAN_RECOVER = True             # True if want recovery
 #TODO: don't recover on first frame
 
 if COMP_SETTINGS:
@@ -43,8 +43,8 @@ frame_rate = 50
 # camera_number = folder + "/test_videos/kabedon_huey.mp4"
 # camera_number = folder + "/test_videos/kabedon_huey.mp4"
 # camera_number = folder + "/test_videos/huey_hell.mp4"
-# camera_number = folder + "/test_videos/huey_bottom_in_n_out.mp4"
-camera_number = 1
+camera_number = folder + "/test_videos/huey_bottom_in_n_out.mp4"
+# camera_number = 1
 
 if IS_TRANSMITTING:
     speed_motor_channel = 1
