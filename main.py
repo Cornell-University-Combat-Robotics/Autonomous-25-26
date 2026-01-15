bd598dd264c80dffa870c91614f1cad429eacdbb
crcfirmware <crcfirmware@gmail.com>
2026-01-15 13:19:02 -0500

    Built new model

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ frame_rate = 30
 # camera_number = folder + "/test_videos/kabedon_huey.mp4"
 # camera_number = folder + "/test_videos/huey_hell.mp4"
 # camera_number = folder + "/test_videos/huey_duet_demo.mp4"
-camera_number = 1
+camera_number = 0
 
 if IS_TRANSMITTING:
     speed_motor_channel = 1
