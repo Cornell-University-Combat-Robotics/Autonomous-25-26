ed381230a44905298aa609dcd67c9cabd38b237c
Riddhi Lamba <rl893@cornell.edu>
2026-03-03 17:56:46 -0500

    converted models for windows

--- machine/convert_models.py
+++ machine/convert_models.py
@@ -6,25 +6,25 @@ models_folder = "./machine/models/"
 
 # model_name = "SmallComp"
 # model_name = "NanoSizeVariant"
-# model_name = "Nano320Temp"
-model_name = "NanoSegHueyPrince"
+model_name = "Nano320Temp"
+# model_name = "NanoSegHueyPrince"
 
 # Smaller number -> Faster
 desired_model_input_size = 320
 
 # CoreML for M-series macs, engine for NVIDIA gpus
-desired_format = "coreml"
+# desired_format = "coreml"
 # desired_format = "engine"
 # desired_format = "onnx"
-# desired_format = "openvino"
+desired_format = "openvino"
 
 base_model_extension = ".pt"
 
 # Load the YOLO model
 model = YOLO(models_folder + model_name + "/" + str(desired_model_input_size) +
-             "/" + model_name + base_model_extension, task='segment')
+             "/" + model_name + base_model_extension, task='detect')
 
 print(model.export(format=desired_format, imgsz=desired_model_input_size,
-      half=True, simplify=True, task='segment'))
+      half=True, simplify=True, task='detect'))
 
 # Terminal prompt: yolo export model=./machine/models/SmallComp/416/SmallComp.pt format=engine simplify=True imgsz=416 half=True
