817190d283b6a1456149c06b7724f44507ffe68c
Aaron Harnish <aaronjharnish@gmail.com>
2026-03-03 01:20:09 -0500

    Removed prints

--- check_number_of_cameras.py
+++ check_number_of_cameras.py
@@ -6,7 +6,7 @@ def get_available_cameras():
     for i in range(5):
         cap = cv2.VideoCapture(i)
         if cap.isOpened():
-            available_cameras.append(cap.)
+            available_cameras.append(i)
             cap.release()
     return available_cameras
 
