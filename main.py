19f76235fffd07ea97a77e38b9ff05bea0dc98cf
Ryan-Siglag <ryan.siglag@gmail.com>
2026-03-15 16:25:45 -0400

    Add purple quant setting profile

--- main.py
+++ main.py
@@ -90,6 +90,7 @@ with open(quant_settings_file, "r") as f:
 # Quantization Settings
 quantization_settings = None
 # quantization_settings = all_settings["Green Huey"]
+# quantization_settings = all_settings["Purple Huey"]
 
 if IS_TRANSMITTING:
     speed_motor_channel = 1
