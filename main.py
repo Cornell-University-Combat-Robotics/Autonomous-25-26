6680a5fe9791c19e1881f8924950b18e42672e60
ethandecamp <decampethanj@gmail.com>
2026-03-21 11:59:50 -0400

    Implemented new PID logic

--- main.py
+++ main.py
@@ -36,10 +36,10 @@ from warp_main import warp_map
 
 # MATT_LAPTOP = False           # Deprecated, matt laptop handled by torch device checks
 JANK_CONTROLLER = False         # Deprecated, True if using backup controller?
-WARP_AND_COLOR_PICKING = False   # Re-do warp & color selection
+WARP_AND_COLOR_PICKING = True   # Re-do warp & color selection
 # Display frame smaller for selection with 1080p video, 1.0 default
 DISPLAY_SCALE = 0.5
-IS_TRANSMITTING = False         # True to send transmissions to live Huey via Arduino
+IS_TRANSMITTING = True         # True to send transmissions to live Huey via Arduino
 WEAPON_ON = False               # True if weapon motor should be on
 SHOW_FRAME = True               # Show camera feed frames
 DISPLAY_ANGLES = True           # Only use when SHOW_FRAME is True
