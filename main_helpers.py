6f12ae9319e1f516ee9b29d02075abd296257be5
Aaron Harnish <aaronjharnish@gmail.com>
2026-04-22 02:35:26 -0400

    Commented out double drawing of circles in display_angles

--- main_helpers.py
+++ main_helpers.py
@@ -238,14 +238,14 @@ def display_angles(detected_bots_with_data, move_dictionary, image, initial_run=
             end_point = (int(start_x + 300 * dx), int(start_y + 300 * dy))
             cv2.arrowedLine(image, (start_x, start_y), end_point, (255, 0, 0), 2)
 
-            # Huey's corner points
-            x_shift = int(detected_bots_with_data["huey"]['bbox'][0][0])
-            y_shift = int(detected_bots_with_data["huey"]['bbox'][0][1])
+            # # Huey's corner points
+            # x_shift = int(detected_bots_with_data["huey"]['bbox'][0][0])
+            # y_shift = int(detected_bots_with_data["huey"]['bbox'][0][1])
 
-            for i in range(len(centroids)):
-                color = (255, 255, 0) if i == 0 else (0, 255, 255)
-                for p in centroids[i]:
-                    cv2.circle(image, (p[0] + x_shift, p[1] + y_shift), 8, color, -1)
+            # for i in range(len(centroids)):
+            #     color = (255, 255, 0) if i == 0 else (0, 255, 255)
+            #     for p in centroids[i]:
+            #         cv2.circle(image, (p[0] + x_shift, p[1] + y_shift), 8, color, -1)
 
                 # for i in range(len(centroids)):
                 # color = (255, 255, 0) if i == 0 else (0, 255, 255)
