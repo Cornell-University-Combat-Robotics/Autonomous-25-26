cc4b7252a77a11c485a49e88bd788111ca354f50
Aaron Harnish <aaronjharnish@gmail.com>
2026-04-24 15:25:10 -0400

    Set to non tracking, seems to be not much difference, live test?

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ BLACKOUT = True
 COLOR_QUANTIZATION = True  # Should almost always stay True
 CAMERA_STREAM = False     # Frame capture thread (must be False for videos)
 IMU_ENABLED = False     # Set to True to enable IMU integration (if hardware is available)
-USE_TRACKING = True       # Use tracking-based predictor instead of running detection on every frame (requires more resources)
+USE_TRACKING = False       # Use tracking-based predictor instead of running detection on every frame (requires more resources)
 
 # Logging / debug outputs
 SHEET_RUNTIME = True
@@ -93,8 +93,8 @@ if MODE == "comp" or MODE == "live":
 elif MODE == "video":
     IS_TRANSMITTING = False         # True to send transmissions to live Huey via Arduino
     WEAPON_ON = False
-    IS_ORIGINAL_FPS = True         # Process every captured frame, False -> cap at FRAME_RATE, only TRUE for Live
-    FRAME_RATE = 60                 # Manually set frame rate for videos
+    IS_ORIGINAL_FPS = False         # Process every captured frame, False -> cap at FRAME_RATE, only TRUE for Live
+    FRAME_RATE = 120                 # Manually set frame rate for videos
     CAMERA_STREAM = False           # True to run frame capture in a seperate thread, always false for videos
 
 elif MODE == "custom":
@@ -108,10 +108,10 @@ else:
 
 folder = os.getcwd() + "/main_files"
 # Video options (uncomment one for MODE = "video")
-# camera_number = folder + "/test_videos/huey_vs_prince.mp4"
+camera_number = folder + "/test_videos/huey_vs_prince.mp4"
 # camera_number = folder + "/test_videos/huey_hell.mp4"
 # camera_number = folder + "/test_videos/huey_in_n_out.mp4"
-camera_number = folder + "/test_videos/orbital_huey.mp4"
+# camera_number = folder + "/test_videos/orbital_huey.mp4"
 
 # Webcam index (used for MODE = "live" or MODE = "comp")
 # camera_number = 0
