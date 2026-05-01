dd383aa03e897c02e295437fdb137ea8f91af7b2
Ananya-Jajodia <nya.jaj@outlook.com>
2026-05-01 19:45:07 -0400

    working live testing code

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ CAN_RECOVER = True
 BLACKOUT = True
 COLOR_QUANTIZATION = True  # Should almost always stay True
 CAMERA_STREAM = True     # Frame capture thread (must be False for videos)
-IMU_ENABLED = False    # Set to True to enable IMU integration (if hardware is available)
+IMU_ENABLED = True    # Set to True to enable IMU integration (if hardware is available)
 USE_TRACKING = True       # Use tracking-based predictor instead of running detection on every frame (requires more resources)
 DETECTION_CONFIDENCE = 0.25  # Ultralytics default is 0.25; Try lower values
 
